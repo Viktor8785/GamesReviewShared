9834ed34324c07438e9127739d4fb419387fef1b
Viktor8785 <v117@list.ru>
2023-07-12 15:52:39 +0300

    tablet icin 2

--- js/game.js
+++ js/game.js
@@ -1,1 +1,102 @@
-import{gamesData}from"./games-data.js";const gameImg=[];gameImg[0]=document.querySelector("#img1"),gameImg[1]=document.querySelector("#img2"),gameImg[2]=document.querySelector("#img3");const cardContainer=document.querySelector(".game__card-container"),gameName=document.querySelector(".game__name"),gameGenre=document.querySelector(".game__genre"),gameText=document.querySelector(".game__text"),buttonBack=document.querySelector(".game__button-back"),buttonWatchlist=document.querySelector(".game__button-watchlist"),buttonSvg=document.querySelector(".game__button-svg"),buttonLeft=document.querySelector(".game__button-left"),buttonRight=document.querySelector(".game__button-right"),watchButton=document.querySelector("#watch-button");let urlParams=new URLSearchParams(window.location.search),param=urlParams.get("param"),game=gamesData.find((t=>t.id==param)),currentIndex=0,leftXs=[];leftXs[0]=0,leftXs[1]=-374,leftXs[2]=-748;let leftX=0;cardContainer.style.left=0;let watchList=localStorage.getItem("watch"),watchListFlag=!1;watchList&&watchList.indexOf(param)>=0&&(buttonSvg.classList.add("game__button-svg--active"),watchListFlag=!0),gameImg[0].src=game.imgMobile1,gameImg[1].src=game.imgMobile2,gameImg[2].src=game.imgMobile3,gameName.textContent=game.name,gameGenre.textContent=game.genre,gameText.textContent=game.text,buttonBack.addEventListener("click",(()=>{window.history.go(-1)})),buttonWatchlist.addEventListener("click",(()=>{buttonSvg.classList.toggle("game__button-svg--active"),watchList=localStorage.getItem("watch");let t="";if(watchListFlag)if(watchListFlag=!1,3==watchList.length)t="";else{let e=watchList.indexOf(param);0==e&&(t=watchList.slice(watchList.indexOf(param)+3)),e==watchList.length-3&&(t=watchList.slice(0,watchList.indexOf(param))),e>0&&e<watchList.length-3&&(t=watchList.slice(0,watchList.indexOf(param))+watchList.slice(watchList.indexOf(param)+3))}else t=param+" ",watchList&&watchList.length>0&&(t=watchList+param+" "),watchListFlag=!0;localStorage.setItem("watch",t)})),buttonLeft.addEventListener("click",(()=>{currentIndex>0&&(currentIndex--,leftX=leftXs[currentIndex],cardContainer.style.left=leftX+"px")})),buttonRight.addEventListener("click",(()=>{currentIndex<2&&(currentIndex++,leftX=leftXs[currentIndex],cardContainer.style.left=leftX+"px")})),watchButton.addEventListener("click",(()=>{watchButton.href="games.html?param=watch"}));
+import {gamesData} from './games-data.js';
+
+const gameImg = [];
+gameImg[0] = document.querySelector('#img1');
+gameImg[1] = document.querySelector('#img2');
+gameImg[2] = document.querySelector('#img3');
+const cardContainer = document.querySelector('.game__card-container');
+const gameName = document.querySelector('.game__name');
+const gameGenre = document.querySelector('.game__genre');
+const gameText = document.querySelector('.game__text');
+const buttonBack = document.querySelector('.game__button-back');
+const buttonWatchlist = document.querySelector('.game__button-watchlist');
+const buttonSvg = document.querySelector('.game__button-svg');
+const buttonLeft = document.querySelector('.game__button-left');
+const buttonRight = document.querySelector('.game__button-right');
+const watchButton = document.querySelector('#watch-button');
+
+let urlParams = new URLSearchParams(window.location.search);
+let param = urlParams.get('param');
+let game = gamesData.find(game => game.id == param);
+let currentIndex = 0;
+
+let leftXs = [];
+leftXs[0] = 0;
+leftXs[1] = -374;
+leftXs[2] = -748;
+let leftX = 0;
+cardContainer.style.left = 0;
+
+let watchList = localStorage.getItem('watch');
+let watchListFlag = false;
+
+if(watchList) {
+  if(watchList.indexOf(param) >= 0) {
+    buttonSvg.classList.add('game__button-svg--active');
+    watchListFlag = true;
+  }
+}
+
+gameImg[0].src = game.imgMobile1;
+gameImg[1].src = game.imgMobile2;
+gameImg[2].src = game.imgMobile3;
+gameName.textContent = game.name;
+gameGenre.textContent = game.genre;
+gameText.textContent = game.text;
+
+buttonBack.addEventListener('click', () => {
+  window.history.go(-1);
+});
+
+buttonWatchlist.addEventListener('click', () => {
+  buttonSvg.classList.toggle('game__button-svg--active');
+  watchList = localStorage.getItem('watch');
+  let watchListNew = '';
+  if(watchListFlag) {
+    watchListFlag = false;
+    if(watchList.length == 3) {
+      watchListNew = '';
+    } else {
+      let indexWatch = watchList.indexOf(param);
+      if(indexWatch == 0) {
+        watchListNew = watchList.slice(watchList.indexOf(param) + 3);
+      }
+      if(indexWatch == watchList.length - 3) {
+        watchListNew = watchList.slice(0, watchList.indexOf(param));
+      }
+      if(indexWatch > 0 && indexWatch < watchList.length - 3) {
+        watchListNew = watchList.slice(0, watchList.indexOf(param))
+        + watchList.slice(watchList.indexOf(param) + 3);
+      }
+    }
+  } else {
+    watchListNew = param + ' ' ;
+    if(watchList) {
+      if(watchList.length > 0) {
+        watchListNew = watchList + param + ' ' ;
+      }
+    }
+    watchListFlag = true;
+  }
+  localStorage.setItem('watch', watchListNew);
+});
+
+buttonLeft.addEventListener('click', () => {
+  if(currentIndex > 0) {
+    currentIndex--;
+    leftX = leftXs[currentIndex];
+    cardContainer.style.left = leftX + 'px';
+  }
+});
+
+buttonRight.addEventListener('click', () => {
+  if(currentIndex < 2) {
+    currentIndex++;
+    leftX = leftXs[currentIndex];
+    cardContainer.style.left = leftX + 'px';
+  }
+});
+
+watchButton.addEventListener('click', () => {
+  watchButton.href = 'games.html?param=watch';
+});
--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-const genreText=document.querySelectorAll(".genre__text"),genreItem=document.querySelectorAll(".genre__item"),genreSvgChecked=document.querySelectorAll(".genre__svg-checked"),genreButton=document.querySelector(".genre__button"),watchButton=document.querySelector("#watch-button");let currentIndex=-1;genreItem.forEach(((e,t)=>{e.addEventListener("click",(()=>{currentIndex>=0&&(genreItem[currentIndex].classList.remove("genre__item--active"),genreSvgChecked[currentIndex].classList.remove("genre__svg-checked--active")),currentIndex=t,e.classList.add("genre__item--active"),genreSvgChecked[t].classList.add("genre__svg-checked--active"),genreButton.classList.add("genre__button--active")}))})),genreButton.addEventListener("click",(()=>{currentIndex>=0&&(genreButton.href="games.html?param="+genreText[currentIndex].textContent)})),watchButton.addEventListener("click",(()=>{watchButton.href="games.html?param=watch"}));
+const genreText = document.querySelectorAll('.genre__text');
+const genreItem = document.querySelectorAll('.genre__item');
+const genreSvgChecked = document.querySelectorAll('.genre__svg-checked');
+const genreButton = document.querySelector('.genre__button');
+const watchButton = document.querySelector('#watch-button');
+
+let currentIndex = -1;
+
+genreItem.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if(currentIndex >= 0) {
+      genreItem[currentIndex].classList.remove('genre__item--active');
+      genreSvgChecked[currentIndex].classList.remove('genre__svg-checked--active')
+    }
+    currentIndex = index;
+    item.classList.add('genre__item--active');
+    genreSvgChecked[index].classList.add('genre__svg-checked--active')
+    genreButton.classList.add('genre__button--active')
+  });
+});
+
+genreButton.addEventListener('click', () => {
+  if(currentIndex >= 0) {
+    genreButton.href = 'games.html?param=' + genreText[currentIndex].textContent;
+  }
+});
+
+watchButton.addEventListener('click', () => {
+  watchButton.href = 'games.html?param=watch';
+});
